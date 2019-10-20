1e1c98a2452feb4600c1c4c995ced388852d3a6c
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-10-20 20:09:26 -0300

    Optionals e uma primeira visão dos seus perigos

--- egg/egg/MyPlayground.playground/Contents.swift
+++ egg/egg/MyPlayground.playground/Contents.swift
@@ -1,31 +1,27 @@
 import UIKit
 
 // Criar primeira refeição
-
 let nome: String = "Macarrão"
 let felicidade: String = "5"
 
 // Criar segunda refeição
-
 let nome2: String = "Churros"
 let felicidade2: String = "4"
-
 
 // ---------------------------
 
 // Criar uma classe que agrupe essas características:
-
+// Optionals para declarar propriedades sem valores
 class Refeicao {
-    let nome: String = "Macarrão"
-    let felicidade: String = "5"
+    var nome: String?
+    var felicidade: String?
 }
 
 // Instanciando uma classe:
+let refeicao = Refeicao()
+refeicao.nome = "Macarrão"
 
-// Explicito
-let refeicao: Refeicao = Refeicao()
+// CUIDADO => Forced Unwrap
+print(refeicao.nome!)
 
-// Implicito
-let refeicao2 = Refeicao()
 
-print(refeicao.nome)
