9fc8d7539373bd7f992bebd00bcfa4e65a971b6d
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-10-20 14:12:37 -0300

    Funções sem e com parâmetros e invocando funções

--- egg/egg/MyPlayground.playground/Contents.swift
+++ egg/egg/MyPlayground.playground/Contents.swift
@@ -13,4 +13,23 @@ let felicidade: Int = 5
 let calorias: Double = 79.5
 let vegetal: Bool = true
 
-print(vegetal)
+// Método Simples
+func alimentoConsumido() {
+    print("O alimento consumido foi: \(nome)")
+}
+
+alimentoConsumido()
+
+// Método com parâmetros
+func alimentoConsumido2(nome: String, caloria: Double) {
+    print("O alimento consumido foi: \(nome), com calorias: \(caloria)")
+}
+
+alimentoConsumido2(nome: nome, caloria: calorias)
+
+// Método com ocultação de parâmetros
+func alimentoConsumido2(_ nome: String, _ caloria: Double) {
+    print("O alimento consumido foi: \(nome), com calorias: \(caloria) - com ocultação")
+}
+
+alimentoConsumido2(nome, calorias)
