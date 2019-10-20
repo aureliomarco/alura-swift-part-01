09b4648861bb9aec8807b97d74ff97c8a9b1f190
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-10-20 13:56:21 -0300

    Comentários code smell double int e Bool

---
+++ egg/egg/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// Comentário de uma única linha
+
+/*
+    mvpmvpmvpmvpmvpmvpmvpmvpmvp
+    mvpmvpmvpmvpmvpmvpmvpmvpmvp
+    mvpmvpmvpmvpmvpmvpmvpmvpmvp
+ */
+
+let nome: String = "churros"
+let felicidade: Int = 5
+let calorias: Double = 79.5
+let vegetal: Bool = true
+
+print(vegetal)
