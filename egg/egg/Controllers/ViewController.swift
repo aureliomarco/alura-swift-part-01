9e64e08c342541e72691f44cf7cf5bd8c38e588c
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-10-21 11:19:14 -0300

    Utilizando orientação a objetos no projeto e tentando limpar alguns optionals

--- egg/egg/Controllers/ViewController.swift
+++ egg/egg/Controllers/ViewController.swift
@@ -14,14 +14,35 @@ class ViewController: UIViewController {
     // Está dizendo ao swift que esta variável terá um valor
     // É a exclamação (!) no final de UITextField
 
-    @IBOutlet var nomeTextField: UITextField!
-    @IBOutlet weak var felicidadeTextField: UITextField!
+    @IBOutlet var nomeTextField: UITextField?
+    @IBOutlet weak var felicidadeTextField: UITextField?
     
     @IBAction func adicionar(_ sender: Any) {
-        let nome = nomeTextField.text
-        let felicidade = felicidadeTextField.text
+        // if let
+        /*
+        if let nomeDaRefeicao = nomeTextField?.text, let felidadeDaRefeicao = felicidadeTextField?.text {
+            let nome = nomeDaRefeicao
+            if let felicidade = Int(felidadeDaRefeicao) {
+                let refeicao = Refeicao(nome: nome, felicidade: felicidade)
+                
+                print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
+            } else {
+                print("Erro ao tentar criar a refeição")
+            }
+        }
+        */
         
-        print("comi \(nome) e fiquei com felicidade: \(felicidade)")
+        // guard let
+        guard let nomeDaRefeicao = nomeTextField?.text else {
+            return
+        }
+        
+        guard let felicidadeDaRefeicao = felicidadeTextField?.text, let felicidade = Int(felicidadeDaRefeicao) else {
+            return
+        }
+        
+        let refeicao = Refeicao(nome: nomeDaRefeicao, felicidade: felicidade)
+        print("comi \(refeicao.nome) e fiquei com felicidade: \(refeicao.felicidade)")
     }
 }
 
