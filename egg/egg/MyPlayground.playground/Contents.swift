be2e0f53b9cb2f37c1934e01aafe42995191ca8a
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-10-21 09:14:49 -0300

    Classe, item, arrays em classes e trabalhando com métodos

--- egg/egg/MyPlayground.playground/Contents.swift
+++ egg/egg/MyPlayground.playground/Contents.swift
@@ -1,23 +1,50 @@
 import UIKit
 
 class Refeicao {
-    
     // MARK: - Atributos
     var nome: String
     var felicidade: String
+    var itens: Array<Item> = []
     
     // MARK: - Construtor
     init(nome: String, felicidade: String) {
         self.nome = nome
         self.felicidade = felicidade
     }
+    
+    // MARK: - Métodos
+    func totalDeCalorias() -> Double {
+        var total = 0.0
+        
+        for item in itens {
+            total += item.calorias
+        }
+        
+        return total
+    }
 }
 
-let refeicao = Refeicao(nome: "Macarrão", felicidade: "5")
-let refeicao2 = Refeicao(nome: "Camarão", felicidade: "1")
+class Item {
+    var nome: String
+    var calorias: Double
+    
+    init(nome: String, calorias: Double) {
+        self.nome = nome
+        self.calorias = calorias
+    }
+}
+
+let arroz = Item(nome: "Arroz", calorias: 90.0)
+let feijao = Item(nome: "Feijão", calorias: 51.0)
+let contraFile = Item(nome: "Contra Filé", calorias: 287.0)
+
+let refeicao = Refeicao(nome: "Almoço", felicidade: "5")
+refeicao.itens.append(arroz)
+refeicao.itens.append(feijao)
+refeicao.itens.append(contraFile)
 
 print(refeicao.nome)
-print(refeicao.felicidade)
-
-print(refeicao2.nome)
-print(refeicao2.felicidade)
+if let primeiroItemDaLista = refeicao.itens.first {
+    print(primeiroItemDaLista.nome)
+}
+print(refeicao.totalDeCalorias())
